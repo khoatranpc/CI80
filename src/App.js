720224a0bbef905d42c3c5324b818eb65e01e5e9
khoatranpc <khoatranpc603@gmail.com>
2023-01-14 22:25:42 +0700

    intro reactjs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="form-task">
+        <input name='task' type="text" placeholder='Type your task here!' className="input-task" />
+        <div className='row'>
+          <input name='todo-task' type="radio" id="clean-up" />
+          <label htmlFor='clean-up'>Clean up bathroom!</label>
+        </div>
+        <div className='row'>
+          <input name='todo-task' type="radio" id="bs-milk" />
+          <label htmlFor='bs-milk'>Buy some milk!</label>
+        </div>
+        <div className='row'>
+          <input name='todo-task' type="radio" id="jogging" />
+          <label htmlFor='jogging'>Jogging!</label>
+        </div>
+        <div className='row'>
+          <input name='todo-task' type="radio" id="learn-react" />
+          <label htmlFor='learn-react'>Learn react!</label>
+        </div>
+      </div>
+      <div className="footer">
+        <div className="left">5 tasks left!</div>
+        <div className="right">MindX todolist</div>
+      </div>
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './Footer.css';
+// rafc
+const Footer = () => {
+    return (
+        <div className="footer">Footer is here</div>
+    )
+}
+export default Footer;
